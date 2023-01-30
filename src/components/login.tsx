8d4a29b879556210770a7f0b24eeaa2ab4903119
Mac Sweeny <macsweenydev@gmail.com>
2023-01-31 03:03:15 +1000

    fix: fixed linting

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,4 @@
-import { useSession, signIn } from "next-auth/react";
-import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 import React from "react";
 const Login: React.FC = () => {
   const session = useSession();
@@ -10,12 +9,12 @@ const Login: React.FC = () => {
         <div className="rounded-md bg-teal-600 px-4 py-2 text-base font-semibold leading-6 text-white duration-300 hover:bg-black hover:bg-discord/80">
           {`Logged in as ${session.data.user?.name}`}
         </div>
-        <Link
-          href="/api/auth/signout"
+        <button
+          onClick={() => signOut()}
           className="rounded-md bg-discord px-4 py-2 text-base font-semibold leading-6 text-white duration-300 hover:bg-black hover:bg-discord/80"
         >
           Logout
-        </Link>
+        </button>
       </div>
     );
   } else {
@@ -23,7 +22,8 @@ const Login: React.FC = () => {
       <div className="flex justify-center">
         <button
           className="flex h-20 w-60 items-center justify-center rounded-md bg-discord  px-4 py-2 text-base font-semibold leading-6 text-black duration-300 hover:bg-discord/80"
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+
+          
           onClick={() => signIn('discord')}
         >
           Login with Discord
